1158b93b2d1a1645f76faa1fc1a091abfbfa3e78
AbrJA <abraham.jaimes.mx@gmail.com>
2025-08-13 00:38:47 -0600

    Enhance normalize method in Model class; add quantiles parameter for flexible quantile calculation

--- R/model.R
+++ R/model.R
@@ -8,18 +8,19 @@
 Model <- R6::R6Class(
   classname = "Model",
   public = list(
-    normalize = function(dt, period) {
+    normalize = function(dt, period, quantiles = c(0.1, 0.9)) {
       checkmate::assertDataTable(dt)
       checkmate::assertInteger(period, lower = 1L)
+      checkmate::assertNumeric(quantiles, len = 2L, lower = 0, upper = 1, sorted = TRUE)
       private$.dt <- data.table::copy(dt)
       private$.dt[, group := period %% period]
       index <- private$.dt[, .I[which.min(value)], by = .(group)]$V1
       private$.dt[index, value := NA_integer_]
-      dt_stats <- private$.dt[, .(value = round(quantile(value, probs = 0.25, na.rm = TRUE))), by = .(group)]
+      dt_stats <- private$.dt[, .(value = round(quantile(value, probs = quantiles[1], na.rm = TRUE))), by = .(group)]
       private$.dt[is.na(value), value := dt_stats[.SD, value, on = .(group)]]
       index <- private$.dt[, .I[which.max(value)], by = .(group)]$V1
       private$.dt[index, value := NA_integer_]
-      dt_stats <- private$.dt[, .(value = round(quantile(value, probs = 0.75, na.rm = TRUE))), by = .(group)]
+      dt_stats <- private$.dt[, .(value = round(quantile(value, probs = quantiles[2], na.rm = TRUE))), by = .(group)]
       private$.dt[is.na(value), value := dt_stats[.SD, value, on = .(group)]]
       private$.dt[, group := NULL]
       private$.serie <- stats::ts(private$.dt$value, start = c(1L, 1L), frequency = period)
